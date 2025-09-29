62a7acd9d99f4bdf649ce99ba03b7455edbaeb8c
fengzi <2798776062@qq.com>
2025-09-29 20:03:50 +0800

    IO流学习

--- src/Demo13.java
+++ src/Demo13.java
@@ -1,6 +1,5 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
 import java.util.function.Consumer;
 
 public class Demo13 {
@@ -11,15 +10,29 @@ public class Demo13 {
         BufferedWriter bw = new BufferedWriter(new FileWriter("sortCsb.txt"));
         //创建集合用于存储读取到的字符串
         ArrayList<String> list = new ArrayList<>();
+        Map<Integer,String> m = new TreeMap<>();//方式二 用双列集合TreeMap实现自动排序
         //调用缓冲字符读入流的readLine()方法逐行读出字符串并存入集合
         String str;
         while ((str = br.readLine()) != null) {
             list.add(str);
+            //方式二
+            m.put(Integer.parseInt(str.split("\\.")[0]),
+                    str.split("\\.")[1]);
         }
         //对list进行排序
-        Collections.sort(list);
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                int i1 = Integer.parseInt(o1.split("\\.")[0]);
+                int i2 = Integer.parseInt(o2.split("\\.")[0]);
+                return i1 - i2;
+            }
+        });
         //查看排序结果
         //System.out.println(list);
+        //方式二：
+        Set<Integer> integers = m.keySet();
+        integers.forEach(i -> System.out.println(m.get(i)));
         //遍历集合 将每一行字符串写入新文件
         list.forEach(s -> {
             try {
